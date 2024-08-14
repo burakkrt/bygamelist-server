c6c253783326f4b480eeb20ea36f73833034393f
burakkrt <burak.kurt@glomil.com>
2024-08-14 09:25:21 +0300

    2 hours token time

--- src/controllers/user/loginUser.ts
+++ src/controllers/user/loginUser.ts
@@ -47,7 +47,7 @@ const loginUser = async (
     }
 
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET!, {
-      expiresIn: process.env.TOKEN_VALIDITY_PERIOD || '1h',
+      expiresIn: process.env.TOKEN_VALIDITY_PERIOD || '2h',
     })
 
     const response: SuccessResponse = {
